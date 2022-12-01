52dea2d81627b749632c1cf98462402c09428688
Sofiane <sofiane.djemaa93@gmail.com>
2022-12-01 11:11:43 +0100

    Addition of DTO and DAO StockStore

---
+++ store/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/StoreStockDAO.java
@@ -0,0 +1,8 @@
+package fr.pantheonsorbonne.ufr27.miage.dao;
+
+import fr.pantheonsorbonne.ufr27.miage.model.StoreStock;
+
+public interface StoreStockDAO {
+
+    StoreStock get(int storeStockID);
+}
